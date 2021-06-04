9cd9730b73a1d77bc3a34b391df7102088c75c80
DigitalEngineeringAEJ <management@aejgmbh.com>
2021-06-04 13:01:41 +0000

    [ADD] Fields für MMD

--- MMD/models/mmd.py
+++ MMD/models/mmd.py
@@ -5,3 +5,15 @@ from odoo import models, fields, api
 class mmd(models.Model):
     _name = 'mmd'
     _description = 'Multi Media'
+    
+    name = fields.Char(string='Name', required='true')
+    beschreibung = fields.Text(string='Beschreibung')
+    
+    auswahl = fields.Selection(string='Level',
+                    selection=[('anfänger', 'Anfänger'),
+                              ('fortgeschrittener','Fortgeschrittener'),
+                              ('profi', 'Profi')],
+                              copy=True)
+    
+    
+    anwesend = fields.Boolean(string='Anwesend',default='true')
